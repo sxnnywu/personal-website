bd9a5857395606aa2f8154289d9a4ce2d9e1cde8
Sunny Wu <w.sunny0618@gmail.com>
2025-08-25 18:07:08 -0400

    exp popups

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -22,90 +22,98 @@ function Experience() {
             company: "International Youth Council on Gender Equality",
             duration: "Aug 2025 - Present",
             whatIDid: [
-                "Founded and led a startup focused on innovative tech solutions.",
-                "Managed a team of developers and designers to deliver high-quality products."
+                "Advise on global programs advancing gender equality, targeting 50 countries.",
+                "Guide leadership on strategy, program direction, and growth.",
+                "Provide input on policy and international collaboration initiatives."
             ],
             howIDid: [
-                "Implemented agile methodologies to streamline project management.",
-                "Fostered a collaborative team environment to encourage creativity and innovation."
+                "Work directly with government officials and UN representatives to align initiatives with global standards.",
+                "Review program strategies and offer actionable recommendations.",
+                "Mentor youth ambassadors on scaling operations and maximizing impact."
             ],
-            impact: [
-                "Successfully launched multiple products, achieving significant user engagement.",
-                "Grew the company from a small startup to a recognized name in the industry."
-            ],
-            skills: ["Leadership", "Project Management", "Agile Methodologies", "Team Building"],
+            impact: [],
+            skills: ["Cross-cultural Communication", "Impact Evaluation", "Strategic Advising"],
         },
         {
             title: "Founder & CEO",
             company: "The Pink Stairs",
             duration: "Jun 2024 - Present",
             whatIDid: [
-                "Founded and led a startup focused on innovative tech solutions.",
-                "Managed a team of developers and designers to deliver high-quality products."
+                "Founded a global youth-led nonprofit advancing gender equity, now active in 26 countries.",
+                "Built and scaled volunteer programs and internal operations for 250+ volunteers.",
+                "Led marketing campaigns reaching 8.4M+ girls."
             ],
             howIDid: [
-                "Implemented agile methodologies to streamline project management.",
-                "Fostered a collaborative team environment to encourage creativity and innovation."
+                "Designed volunteer onboarding, training, and program management systems.",
+                "Develop and execute cross-platform marketing strategies.",
+                "Organize seasonal campaigns for crisis relief, career growth, and financial literacy."
             ],
             impact: [
-                "Successfully launched multiple products, achieving significant user engagement.",
-                "Grew the company from a small startup to a recognized name in the industry."
+                "Delivered 570+ letters and 820+ care packages to survivors.",
+                "Educating youth through blogs, magazine, and podcast.",
+                "Grew social media following to 10,000+, increasing engagement and awareness."
             ],
-            skills: ["Leadership", "Project Management", "Agile Methodologies", "Team Building"],
+            skills: ["Google Workspace", "Growth Strategy", "Slack", "Project Management", "Partner Negotiations"],
         },
         {
             title: "Accounting Intern",
             company: "Skans Accountants",
             duration: "Feb 2024 - Jun 2024",
             whatIDid: [
-                "Founded and led a startup focused on innovative tech solutions.",
-                "Managed a team of developers and designers to deliver high-quality products."
+                "Managed financial records and supported bookkeeping for 15+ clients.",
+                "Prepared 10+ personal and corporate tax returns ensuring CRA compliance.",
+                "Coordinated property tax rebate claims for residential and commercial clients."
             ],
             howIDid: [
-                "Implemented agile methodologies to streamline project management.",
-                "Fostered a collaborative team environment to encourage creativity and innovation."
+                "Used Excel and QuickBooks to maintain and reconcile financial records.",
+                "Reviewed tax forms to identify deductions and maximize refunds."
             ],
             impact: [
-                "Successfully launched multiple products, achieving significant user engagement.",
-                "Grew the company from a small startup to a recognized name in the industry."
+                "Ensured 100% compliance with tax regulations.",
+                "Recovered $2,000 in property tax rebates.",
+                "Streamlined bookkeeping and tax processes for multiple clients."
             ],
-            skills: ["Leadership", "Project Management", "Agile Methodologies", "Team Building"],
+            skills: ["QuickBooks", "Microsft Office Suite", "Bookkeeping", "Tax Compliace", "Corporate Accounting"],
         },
         {
             title: "Frontend Supervisor",
             company: "No Frills",
             duration: "Aug 2023 - Mar 2024",
             whatIDid: [
-                "Founded and led a startup focused on innovative tech solutions.",
-                "Managed a team of developers and designers to deliver high-quality products."
+                "Supervised 20+ cashiers and trained employees on POS and cash handling procedures.",
+                "Prepared and balanced daily cash reports and bank deposits.",
+                "Handled and safeguarded $10,000+ in cash."
             ],
             howIDid: [
-                "Implemented agile methodologies to streamline project management.",
-                "Fostered a collaborative team environment to encourage creativity and innovation."
+                "Implemented standard operating procedures for cash management and security.",
+                "Monitored team performance and coached for efficiency and accuracy.",
+                "Ensured compliance with company policies and loss prevention measures.",
+                "Optimized task delegation for cashiers based on customer flow, balancing workloads."
             ],
             impact: [
-                "Successfully launched multiple products, achieving significant user engagement.",
-                "Grew the company from a small startup to a recognized name in the industry."
+                "Reduced $300 in theft attempts and improved accuracy in cash handling.",
+                "Saved on average 50% overtime for closing cashiers while maintaining task completion rates.",
+                "Developed team skills and knowledge, improving customer experience."
             ],
-            skills: ["Leadership", "Project Management", "Agile Methodologies", "Team Building"],
+            skills: ["Customer Service", "Workflow Planning", "POS systems", "Loss Prevention Strategy", "Report Reconciliation", "Team Supervision"],
         },
         {
             title: "Cashier",
             company: "No Frills",
             duration: "Aug 2022 - Jul 2023",
             whatIDid: [
-                "Founded and led a startup focused on innovative tech solutions.",
-                "Managed a team of developers and designers to deliver high-quality products."
+                "Enforced security protocols and company policies while serving 60+ customers per hour.",
+                "Memorized and processed 100+ food codes and barcodes efficiently."
             ],
             howIDid: [
-                "Implemented agile methodologies to streamline project management.",
-                "Fostered a collaborative team environment to encourage creativity and innovation."
+                "Used POS systems to scan items, process payments, and manage returns.",
+                "Assisted customers with inquiries and ensured smooth checkout operations."
             ],
             impact: [
-                "Successfully launched multiple products, achieving significant user engagement.",
-                "Grew the company from a small startup to a recognized name in the industry."
+                "Maintained fast, accurate checkout service and customer satisfaction.",
+                "Reduced errors and losses through strict adherence to procedures."
             ],
-            skills: ["Leadership", "Project Management", "Agile Methodologies", "Team Building"],
+            skills: ["Customer Service", "Communication", "POS systems", "Cash handling", "Problem-solving"],
         },
     ]
 
