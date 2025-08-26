54b22151ab4990fafca9eda2cde340fae4bf45ad
Sunny Wu <w.sunny0618@gmail.com>
2025-08-26 00:51:42 -0400

    projects loop incomplete

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,14 +1,17 @@
 // IMPORTS
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-coverflow';
 import { EffectCoverflow, Pagination } from 'swiper/modules'
 import './Projects.css';
+import { m } from 'framer-motion';
 
 // COMPONENT
 function Projects() {
+
+    const swiperRef = useRef(null);
 
     const projects = [
         {
@@ -19,22 +22,22 @@ function Projects() {
         },
         {
             title: "QueueMe",
-            description: "Reducing physical waiting times through an online queueing system",
+            description: "Mobile app digitizing physical lines for service providers and clients",
             stack: "react native, typescript, python, flask",
             link: "#"
         },
         {
-            title: "Bohanza",
-            description: "Digitized card game utilizing heuristics",
-            stack: "java",
+            title: "Nom Nom",
+            description: "DMZ backed discreet safety app for domestic violence, disguised as a bakery",
+            stack: "react native, typescript, node js, express, mongodb, aws, callkit",
             link: "#"
         },
         {
-            title: "QueueMe",
-            description: "Reducing physical waiting times through an online queueing system",
-            stack: "react native, typescript, python, flask",
+            title: "Chalkboard",
+            description: "Desktop app designed to streamline student club organization in a high school or university setting",
+            stack: "java, sqlite",
             link: "#"
-        }
+        },
     ]
 
     return (
@@ -47,17 +50,19 @@ function Projects() {
             </p>
 
             <Swiper
+                onSwiper={swiper =>(swiperRef.current = swiper)}
                 effect="coverflow"
                 grabCursor={true}
                 centeredSlides={true}
                 slidesPerView="auto"
                 loop={true}
+                speed={500}
                 coverflowEffect={{
                     rotate: 20,
-                    stretch: 50,
-                    depth: 250,
-                    modifier: 1,
-                    slideShadows: false,
+                    stretch: 0,
+                    depth: 220,
+                    modifier: 2,
+                    slideShadows: true,
                 }}
                 pagination={{ clickable: true }}
                 modules={[EffectCoverflow, Pagination]}
@@ -65,7 +70,10 @@ function Projects() {
             >
                 {projects.map((proj, index) => (
                     <SwiperSlide key={index} className="project-slide">
-                        <div className="project-card">
+                        <div 
+                            className="project-card" 
+                            onClick={() => swiperRef.current.slideTo(index, 500)}
+                        >
                             <h3>{proj.title}</h3>
                             <p>{proj.description}</p>
                             {proj.link && (
