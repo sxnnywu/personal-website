e69362273941d8d0a0cb04a6b90ca6991882a14e
Sunny Wu <w.sunny0618@gmail.com>
2025-08-27 17:38:44 -0400

    features done

--- src/components/FeatureItem.jsx
+++ src/components/FeatureItem.jsx
@@ -1,26 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './FeatureItem.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 function FeatureItem({ features = [] }) {
-
-    useEffect(() => {
-        AOS.init({ duration: 800, once: true, offset: 600 });
-        AOS.refreshHard();
-    }, []);
-
     return (
         <div className='feature-item'>
 
             {features.map((feature, index) => (
                 <div
                     onClick={() => {
-                        window.open(feature.link, "_blank", "noopener,noreferrer")}}
-                        data-aos="fade-up"
-                        data-aos-delay={index * 200}
-                        data-aos-anchor-placement="top-bottom"
+                        window.open(feature.link, "_blank", "noopener,noreferrer");
+                    }}
                     key={index}
+                    style={{ '--index': index }}
                 >
                     <img src={feature.image} alt='thumbnail' />
                     <h3>{feature.platform}</h3>
