dd0d3899687b8476b45a1da54a935a9a63575145
Sunny Wu <w.sunny0618@gmail.com>
2025-08-27 22:46:34 -0400

    be test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
-import app from "./app.js";
+import app, { connectDB } from "./app.js";
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+connectDB().then(() => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => {
+  console.error("Failed to connect to DB:", err);
+  process.exit(1);
 });
